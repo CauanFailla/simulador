13720aa0500e48b51dea26b3f624cdf6480b6252
cauanfailla <101191360+CauanFailla@users.noreply.github.com>
2023-04-02 15:56:22 -0300

    atualizando

--- script.js
+++ script.js
@@ -21,6 +21,9 @@ let car3 = document.querySelector('.car_3')
 let cars = [car1,car2,car3]
 
 let viajem1 = document.querySelector('.viajem_1')
+let viajem2 = document.querySelector('.viajem_2')
+let viajem3 = document.querySelector('.viajem_3')
+let viajens = [viajem1,viajem2,viajem3]
 
 /*Compras */
 let cars_din = [525000,200000,10000]
@@ -33,8 +36,11 @@ for (let c in cars) {
 
 let pertences = []
 
-let viajem_din = [17355]
-viajem1.innerHTML += `<strong>${new Intl.NumberFormat('pt-BR',{style:'currency',currency:'BRL'}).format(viajem_din[0])}<strong>`
+let viajem_din = [17355,35000,25000]
+for (let c in viajens) {
+    viajens[c].innerHTML += `<strong>${new Intl.NumberFormat('pt-BR',{style:'currency',currency:'BRL'}).format(viajem_din[c])}<strong>`
+}
+
 
 
 
@@ -149,15 +155,16 @@ function investir() {
 
 
 function viajar() {
-    sec_viajar.style.display = 'block'
+    sec_viajar.style.display = 'flex'
     sec_compra.style.display = 'none'
     sec_pertences.style.display = 'none'
     def_din.style.display = 'none'
 }
 
 function pagar_viagem_1() {
-    if (viagem_validacao(inpd.value))  {
-        alert('Você não tem dinheiro suficiente!')
+    let viajem_val = 'via1' 
+    if (viagem_validacao(inpd.value,viajem_val))  {
+        alert(`Você não tem dinheiro suficiente, será necessario R$${viajem_din[0]}`)
     } else {
         inpd.value = inpd.value - viajem_din[0]
         btn_din.innerHTML = (`${new Intl.NumberFormat('pt-BR',{style:'currency',currency:'BRL'}).format(inpd.value)}`)
@@ -165,15 +172,54 @@ function pagar_viagem_1() {
     }
 }
 
-function viagem_validacao(d) {
-    if  (Number(d) < viajem_din[0])  {
-        return  true
+function pagar_viagem_2() {
+    let viajem_val = 'via2' 
+    if (viagem_validacao(inpd.value,viajem_val))  {
+        alert(`Você não tem dinheiro suficiente, será necessario R$${viajem_din[1]}`)
     } else {
-        return false
+        inpd.value = inpd.value - viajem_din[1]
+        btn_din.innerHTML = (`${new Intl.NumberFormat('pt-BR',{style:'currency',currency:'BRL'}).format(inpd.value)}`)
+
     }
+
 }
 
+function pagar_viagem_3() {
+    let viajem_val = 'via3' 
+    if (viagem_validacao(inpd.value,viajem_val))  {
+        alert(`Você não tem dinheiro suficiente, será necessario R$${viajem_din[2]}`)
+    } else {
+        inpd.value = inpd.value - viajem_din[2]
+        btn_din.innerHTML = (`${new Intl.NumberFormat('pt-BR',{style:'currency',currency:'BRL'}).format(inpd.value)}`)
 
+    }
+
+}
+ 
+function viagem_validacao(d,v) {
+    if (v == 'via1') {
+        if  (Number(d) < viajem_din[0])  {
+            return  true
+        } else {
+            return false
+        }
+    } else  if (v == 'via2') {
+        if  (Number(d) < viajem_din[1])  {
+            return  true
+        } else {
+            return false
+        }
+    } else if (v == 'via3') {
+        if  (Number(d) < viajem_din[2])  {
+            return  true
+        } else {
+            return false
+        }
+    }
+    
+}
+
+let bd = document.body
 function meus_pertences() {
     sec_compra.style.display = 'none'
     sec_viajar.style.display = 'none'
@@ -183,23 +229,63 @@ function meus_pertences() {
     for (let c in pertences) {
        
         let per = document.createElement('div')
-        per.innerHTML = `${pertences[c]}`
         if  (pertences[c] == 'Chevete') {
             let img_che = document.createElement('img')
+            let venda = document.createElement('button')
+            venda.innerHTML = `Vender`
             img_che.setAttribute('src','imagens/cheve.webp')
+            venda.addEventListener('click',vender)
             per.appendChild(img_che)
+            per.appendChild(venda)
+
+            function vender() {
+                inpd.value = Number(inpd.value) + Number(5000)
+                btn_din.innerHTML = (`${new Intl.NumberFormat('pt-BR',{style:'currency',currency:'BRL'}).format(inpd.value)}`)
+                venda.style.display = 'none'
+                img_che.setAttribute('src','')
+                pertences.splice(pertences.indexOf('Chevete'),1)
+            }
+
+            
         }
         if  (pertences[c] == 'Kuruma') {
             let img_che = document.createElement('img')
+            let venda_3 = document.createElement('button')
+            venda_3.innerHTML = `Vender`
+            venda_3.addEventListener('click',vender_3)
             img_che.setAttribute('src','imagens/kuru.jpg')
             per.appendChild(img_che)
+            per.appendChild(venda_3)
+
+            function vender_3() {
+                inpd.value = Number(inpd.value) + Number(250000)
+                btn_din.innerHTML = (`${new Intl.NumberFormat('pt-BR',{style:'currency',currency:'BRL'}).format(inpd.value)}`)
+                venda_3.style.display = 'none'
+                img_che.setAttribute('src','')
+                pertences.splice(pertences.indexOf('Kuruma'),1)
+            }
         }
         if  (pertences[c] == 'Skyline') {
             let img_che = document.createElement('img')
+            let venda_2 = document.createElement('button')
+            venda_2.innerHTML = `Vender`
+            venda_2.addEventListener('click',vender_2)
             img_che.setAttribute('src','imagens/sky.jpg')
             per.appendChild(img_che)
+            per.appendChild(venda_2)
+
+            function  vender_2() {
+                inpd.value = Number(inpd.value) + Number(50000)
+                btn_din.innerHTML = (`${new Intl.NumberFormat('pt-BR',{style:'currency',currency:'BRL'}).format(inpd.value)}`)
+                venda_2.style.display  = 'none'
+                img_che.setAttribute('src','')
+                pertences.splice(pertences.indexOf('Skyline'),1)
+                console.log(pertences)
+            }
         }
         div_pertences.appendChild(per)
        
     }
 }
+
+
